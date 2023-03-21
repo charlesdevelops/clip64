b8b795b4cad936905d719b7160f804c51b2a8bd4
Charles <charlesaidanwatson@icloud.com>
2023-03-21 15:50:53 +0800

    implemented decoder and output to clipboard

--- clip64.java
+++ clip64.java
@@ -1,5 +1,22 @@
+import java.util.Base64;
+import java.util.Scanner;
+import java.awt.datatransfer.StringSelection;
+import java.awt.Toolkit;
+
 public class clip64 {
   public static void main(String[] args){
-      ;;;
+      Scanner sc = new Scanner(System.in);
+      Base64.Decoder decoder = Base64.getDecoder();
+
+      String decodedString = new String(decoder.decode(args[0]));
+
+      Toolkit.getDefaultToolkit()
+        .getSystemClipboard()
+        .setContents(
+                new StringSelection(decodedString),
+                null
+      );
+
+      System.out.println("[" + decodedString + "]" + " copied to clipboard");
   }
 }
